96c0c2d0cfdb603269b413bcc5e8383d61b6202a
Apsara <paneruapsara831ap@gmail.com>
2024-06-02 21:59:25 +0545

    sign /up

--- React-Proj/react-project/src/App.jsx
+++ React-Proj/react-project/src/App.jsx
@@ -1,55 +1,17 @@
-// import { CiMail } from "react-icons/ci";
-// import Image from "./assets/Images/first.jpg"
-// import Home from "./components/Home"
+
+import HomeP from "./components/common/HomeP";
+import Signup from "./components/common/Signup";
+
 function App() {
 
   return (
     <>
-     {/* <header className="bg-[#7E33E0] text-white p-1 pl-[314px] flex">
-      
+    <HomeP/>
+    <Signup/>
+     
+ </>
+ )
+ }
 
-      <CiMail className="h-8"/> &nbsp;apsarapaneru@gmail.com &nbsp;<CiMail className="h-8"/> +977643587
-      
-      <div className="mr-[444px]">
-        <ul className="flex space-x-9">
-          <li>English</li>
-          <li>USD</li>
-          <li>Login</li>
-          <li>Wishlist</li>
-        </ul>
-      </div>
-      </header>
-      <nav className="flex h-44px pl-[290px]">
-        <p className="mr-12 ">HEKTO</p>
-        <ul className=" flex">
-        <li>Home</li>
-        <li>Page</li>
-
-        <li>Products</li>
-        <li>Blog</li>
-        <li>Shopping</li>
-        </ul></nav>
-        <body>
-          <div className=" font-bold text-2xl text-pink outline-4 outline-slate-500">
-        <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque esse quod voluptatem iure cumque quaerat reprehenderit officia velit error sequi doloremque ratione dolor sed, 
-          iusto eveniet voluptate architecto reiciendis veritatis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis quaerat ipsum reiciendis amet doloremque perferendis perspiciatis dolores, eligendi,
-           doloribus autem reprehenderit consequuntur laudantium natus odit laboriosam sit temporibus at aliquam.
-          </p>
-          </div>
-          <div>
-            <ul><li>Products</li></ul>
-            <ul><li>Products</li></ul>
-            <ul><li>Products</li></ul>
-            <ul><li>Products</li></ul>
-            <ul><li>Products</li></ul>
-            <ul><li>Products</li></ul>
-          </div> */}
- 
-          {/* <Home/> */}
-       
-    </>
-    
-  )
-}
 
 export default App
